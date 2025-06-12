df52dea3fec05913070d602c917a07a05124a2d8
Ammarah Mirani <ammaramirani135@gmail.com>
2025-06-12 16:59:49 +0500

    initial commit

---
+++ box.cpp
@@ -0,0 +1,165 @@
+#include<iostream>
+#include<stdlib.h>
+#include<unistd.h>
+using namespace std;
+void F1();
+void F2();
+void F3();
+void F4();
+void F5();
+void F6();
+void F7();
+void F8();
+void F9();
+void F10();
+
+int main()
+{
+int input;
+cout<<"\n How many times do you want the box should rotate ?";
+cin>>input;
+while(input)
+{
+
+
+    F1();
+    usleep(1000000); 
+    system("clear");
+     F2();
+      usleep(1000000); 
+    system("clear");
+      F3();
+      usleep(1000000); 
+    system("clear");
+       F4();
+       usleep(1000000); 
+    system("clear");
+        F5();
+       usleep(1000000); 
+    system("clear");
+         F6();
+         usleep(1000000); 
+    system("clear");
+          F7();
+          usleep(1000000); 
+    system("clear");
+           F8();
+           usleep(1000000); 
+    system("clear");
+            F9();
+            usleep(1000000); 
+    system("clear");
+             F10();
+usleep(1000000); 
+    system("clear");
+
+    input--;
+}
+
+return 0;
+}
+void F1()
+{
+
+    cout<<"\n^^^^^^^^^^^";
+    cout<<"\n^^^^^^^^^^^";
+    cout<<"\n^^^^^^^^^^^";
+    cout<<"\n^^^^^^^^^^^";
+    cout<<"\n^^^^^^^^^^^";
+
+}
+
+  void F2()
+{
+
+    cout<<"\n   ^^^^^^^^^^^";
+    cout<<"\n   ^^^^^^^^^^^";
+    cout<<"\n   ^^^^^^^^^^^";
+    cout<<"\n   ^^^^^^^^^^^";
+    cout<<"\n   ^^^^^^^^^^^";
+
+}
+void F3()
+{
+
+    cout<<"\n      ^^^^^^^^^^^";
+    cout<<"\n      ^^^^^^^^^^^";
+    cout<<"\n      ^^^^^^^^^^^";
+    cout<<"\n      ^^^^^^^^^^^";
+    cout<<"\n      ^^^^^^^^^^^";
+
+}
+
+void F4()
+{
+
+    cout<<"\n         ^^^^^^^^^^^";
+    cout<<"\n         ^^^^^^^^^^^";
+    cout<<"\n         ^^^^^^^^^^^";
+    cout<<"\n         ^^^^^^^^^^^";
+    cout<<"\n         ^^^^^^^^^^^";
+
+}
+void F5()
+{
+
+    cout<<"\n            ^^^^^^^^^^^";
+    cout<<"\n            ^^^^^^^^^^^";
+    cout<<"\n            ^^^^^^^^^^^";
+    cout<<"\n            ^^^^^^^^^^^";
+    cout<<"\n            ^^^^^^^^^^^";
+
+}
+void F6()
+{
+
+    cout<<"\n               ^^^^^^^^^^^";
+    cout<<"\n               ^^^^^^^^^^^";
+    cout<<"\n               ^^^^^^^^^^^";
+    cout<<"\n               ^^^^^^^^^^^";
+    cout<<"\n               ^^^^^^^^^^^";
+
+}
+
+void F7()
+{
+
+    cout<<"\n                     ^^^^^^^^^^^";
+    cout<<"\n                     ^^^^^^^^^^^";
+    cout<<"\n                     ^^^^^^^^^^^";
+    cout<<"\n                     ^^^^^^^^^^^";
+    cout<<"\n                     ^^^^^^^^^^^";
+
+}
+
+void F8()
+{
+
+    cout<<"\n                               ^^^^^^^^^^^";
+    cout<<"\n                               ^^^^^^^^^^^";
+    cout<<"\n                               ^^^^^^^^^^^";
+    cout<<"\n                               ^^^^^^^^^^^";
+    cout<<"\n                               ^^^^^^^^^^^";
+
+}
+
+void F9()
+{
+
+    cout<<"\n                                        ^^^^^^^^^^^";
+    cout<<"\n                                        ^^^^^^^^^^^";
+    cout<<"\n                                        ^^^^^^^^^^^";
+    cout<<"\n                                        ^^^^^^^^^^^";
+    cout<<"\n                                        ^^^^^^^^^^^";
+
+}
+  void F10()
+{
+
+    cout<<"\n                                                ^^^^^^^^^^^";
+    cout<<"\n                                                ^^^^^^^^^^^";
+    cout<<"\n                                                ^^^^^^^^^^^";
+    cout<<"\n                                                ^^^^^^^^^^^";
+    cout<<"\n                                                ^^^^^^^^^^^";
+
+}
